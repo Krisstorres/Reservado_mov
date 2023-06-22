529ce90a2dbbaf528f04c86c4c97dda92c0c0a0f
Krisstorres <cristoes01@hotmail.com>
2023-06-22 01:17:15 -0400

    Extraccion de datos Primordiales Realizada correctamente

--- reservado/src/app/services/api.service.ts
+++ reservado/src/app/services/api.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as jwt from 'jsonwebtoken';
+import { resolve } from 'dns';
+
 @Injectable({
   providedIn: 'root'
 })
+
+//API service manager 
 export class ApiService {
   ruta: string = 'http://34.125.248.124:8000/api/auth/';
   edificios='http://127.0.0.1:5000/api/edificio';
-  rutaComunidad='http://34.125.248.124:8000/api/comunidades/1';
+  rutaComunidad='http://34.125.248.124:8000/api/comunidades';
+  rutaEspacioComun='http://34.125.248.124:8000/api/espaciocomun/'
+  rutaDetalleEspacioComun='http://34.125.248.124:8000/api/ecdetail/'
+  rutaConsultarReservas='http://34.125.248.124:8000/api/reservas';
   validador: boolean = false
   
   constructor(
@@ -18,7 +25,7 @@ export class ApiService {
 
   }
 
-
+//Validar Credenciales 
   ValidateApiService(correo: string, contra: string) {
     let that = this;
     const body = {
@@ -29,10 +36,9 @@ export class ApiService {
       resolve(that.http.post(that.ruta, body).toPromise())
     })
   }
+//Validar Credenciales 
 
-  
-
-
+//Listar Edificios 
 getData() {
     let that = this;
     const gruposs='2'
@@ -46,17 +52,59 @@ getData() {
     })
   }
 
+//Listar Edificios 
 
 
-  //GET COMININDAD 
-  listComunidad(token:any){
+
+  //Listar Comunidad
+  listComunidad(){
     let that= this;
   
+    
     return new Promise(resolve => {
-      resolve(that.http.get(that.rutaComunidad,token).toPromise())
+      resolve(that.http.get(that.rutaComunidad).toPromise())
     });
   }
+  //Listar Comunidad
+  
 
-  //GET COMININDAD 
+
+
+//Listar Espacio Común
+  listEspacios(){
+    let that=this;
+
+
+    return new Promise(resolve => {
+      resolve(that.http.get(that.rutaEspacioComun).toPromise())
+    });
+  }
+//Listar Espacio Común
+
+
+//Listar detalle de espacioComun
+getDetalleEspacio( id:string){
+  let that= this;
+
+  return new Promise(resolve => {
+    resolve(that.http.get(that.rutaDetalleEspacioComun+id).toPromise())
+  });
+};
+//Listar detalle de espacioComun
+
+
+
+//Listar reservas (para validaciones )
+getReservas(){
+  let that=this;
+
+
+  return new Promise(resolve =>{
+    resolve(that.http.get(that.rutaConsultarReservas).toPromise())
+  });
+};
+//Listar reservas (para validaciones )
+
+
 }
-
+//API service manager 
--- reservado/src/app/tab1/tab1.page.ts
+++ reservado/src/app/tab1/tab1.page.ts
@@ -10,41 +10,47 @@ import * as jwt from 'jsonwebtoken';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+//Logica Pagina Tab1
   export class Tab1Page implements OnInit {
-    
+//Variables 
+  mensaje:string='';
   corre:any;
   contra:any;
   tokken:any;
   co:string='';
   con:string='';
   tok:string='';
-  
-  mensaje: string='';
-  calle:string='';
-  comuna:string='';
+
   id:string='';
   nombre:string='';
+  calle:string='';
   numeracion:string='';
+  comuna:string='';
   owned_by:string='';
-  list1=[];
-  Edificios=[
-    this.nombre.split(','),
-    this.calle.split(','),
-    this.numeracion.split(','),
-    this.comuna
-  ]
- 
-//Para hmtl
-  // "calle": "Apocalipsis,Clotetzuxal,Lápidas",
-  //   "comuna": "21,30,38",
-  //   "id": "1,2,3",
-  //   "mensaje": "Funcionó",
-  //   "nombre": "Babel,Bizantino,Necrópolis",
-  //   "numeracion": "661,555,888",
-  //   "owned_by": "3,3,3"
+  
+  direccion_completa:string='';
+  nombre_comunidad:string='';
+  comuna_comunidad :string='';
+  datos:any=[];
+  input_datos:any=[];
+  currentFood:any = undefined;
+  currentFood2:any = undefined;
+  currentFood3:any=undefined;
+  espaciosComunes:any=[];
+  esVacio:boolean=false;
+  idComunidad:string='';
+  idEspacioComun:string='';
+  periodos:any=[
 
- 
+  ];
+  perdiodos_duro=['09:00-09:30','10:00-10:30','11:00-11:30','12:00-12:30','13:00-13:30','14:00-14:30','15:00-15:30'];
+  esNull:boolean =false;
+  dateTime:string='';
+  test_print:string='';
+  hora:string='';
+//Variables 
 
+  //Inyeccion de componentes  y servicios 
   constructor(
       
     private router:Router
@@ -52,10 +58,14 @@ import * as jwt from 'jsonwebtoken';
    ,private toastController:ToastController
    ,private loadingCtrl: LoadingController
               
-              ) {}
+              ) {};
+//Inyeccion de componentes  y servicios               
 
-  ngOnInit() {
+  
+//Funciones y variables de arranque 
+ngOnInit() {
     try{
+      //Sacando informacion del Navigation Extras 
       this.corre= this.router.getCurrentNavigation();
       this.co= this.corre.extras.state.correo.toString();
       this.contra= this.router.getCurrentNavigation();
@@ -63,7 +73,8 @@ import * as jwt from 'jsonwebtoken';
       this.tokken= this.router.getCurrentNavigation();
       this.tok= this.tokken.extras.state.token_a.toString();
       console.log('Usuario :'+this.co+"\nContraseña: "+this.con+"\nToken: "+this.tok);
-      // token:string='';
+      //Sacando informacion del Navigation Extras 
+      
     }
     catch(error){
     console.log('Error de navigations extras '+error)      
@@ -76,6 +87,9 @@ import * as jwt from 'jsonwebtoken';
 
     };
 };
+//Funciones y variables de arranque 
+
+//Cargando Toast controller
 async presentToast(mensaje:string) {
   const toast = await this.toastController.create({
   message: mensaje,
@@ -85,71 +99,156 @@ async presentToast(mensaje:string) {
   
   await toast.present();
   }
+//Cargando Toast controller
 
-
-
+//Funcion de carga de arranque para extraccion de datos 
 prueba(){
-
 this.getComunidad();
-this.getEdificio();
+this.getEspacios();
 }
+//Funcion de carga de arranque para extraccion de datos 
 
-async getEdificio(){
-  const that=this;
+//Funcion de Testeo TEMPORAL 
+test(){
+  const fecha=this.test_print.split('T')[0].replace('-','/').replace('-','/')
+  const anno=fecha.split('/')[0];
+  const mes=fecha.split('/')[1];
+  const dia=fecha.split('/')[2];
+  const hora=this.test_print.split('T')[1]
+  this.hora=hora;
+  this.test_print=''.concat(dia).concat('/'+mes).concat('/'+anno);
+  console.log('Fecha: '+this.test_print+"\nHora: "+this.hora);
+  const dateString = "2023-06-24T18:00:00.744000-04:00";
+const dateObj = new Date(dateString);
 
-  try{
-    let respons:any=await that.api.getData()
-    if (respons["mensaje"]==="Funcionó"){
-   
-      console.log('funcionoooooooooooooooooooooooooooooooooooooooooooooooo');
-      this.loading_controll();
-      this.mensaje=respons["mensaje"];
-      this.calle=respons["calle"];
-      this.comuna=respons["comuna"];
-      this.id=respons["id"];
-      this.nombre=respons["nombre"];
-      this.numeracion=respons["numeracion"];
-      this.owned_by=respons["owned_by"];
-      this.Edificios=[
-        this.nombre,
-        this.calle,
-        this.numeracion,
-        this.comuna
-      ]; 
-     console.log('mensaje: '+this.mensaje+"\nCalle: "+this.calle+"\nComuna: "+this.comuna+"\nID: "+this.id+"\nNombre: "+this.nombre+"\nNumeracion: "+this.numeracion+"\nOwned_by: "+this.owned_by);
-      
-    }else{
-      console.log('Error en datos '+respons["mensaje"])
-    }
-
-  }catch(error){
-    console.log("Error Api: "+error +"c")
-  }
+console.log(dateObj);
 }
+//Funcion de Testeo TEMPORAL 
 
+//Preseteo de mensajes de Loading controller 
 async loading_controll() {
   console.log('Loading controller')
   const loading = await this.loadingCtrl.create({
-  message: 'Validando información',
+  message: 'Enviando Informacion !',
   duration: 3000,
   });
   
   loading.present();
   }
+  async loading_controlle() {
+    console.log('Loading controller')
+    const loading = await this.loadingCtrl.create({
+    message: 'No pudimos procesar la información :C',
+    duration: 3000,
+    });
+    
+    loading.present();
+    }
+//Preseteo de mensajes de Loading controller 
+
+//Funcion Listar Comunidades (APISERVICE)
 async getComunidad(){
   const that=this;
   
   try{
     const tokem=that.tok
-    const body={
-      "Headers" : "Authorization : Bearer "+tokem
-    };
-    console.log(body)
-    const data:any =await that.api.listComunidad(body);
-    console.log('Funcooooo'+data)
+    
+    
+    const data:any =await that.api.listComunidad();
+
+    this.datos=data;
+    this.id=this.datos.id;
+
+
 
   }catch(error){
     console.log('Error api:'+error)
   }
 }
+//Funcion Listar Comunidades (APISERVICE)
+
+//Funcion Listar Espacios Comunes (APISERVICE)
+get_espacio(){
+  
+  console.log("Select-options: "+this.input_datos);
+};
+//Funcion Listar Espacios Comunes (APISERVICE)
+
+//Funcion Obtencion de datos de html Options
+compareWith(o1:any, o2:any) {
+  return o1 && o2 ? o1.id === o2.id : o1 === o2;
+};
+
+handleChange(ev:any) {
+  this.currentFood = ev.target.value;
+  this.input_datos=this.currentFood
+  console.log('Input dataa '+this.input_datos["id"]);
+  this.idComunidad=this.input_datos["id"];
+};
+  compareWith2(o1:any, o2:any) {
+    return o1 && o2 ? o1.id === o2.id : o1 === o2;
+  };
+
+  handleChange2(ev:any) {
+    this.currentFood2 = ev.target.value;
+    this.input_datos=this.currentFood3
+    
+    console.log('Input dataa '+this.currentFood2)
+  };
+  //Funcion Obtencion de datos de html Options
+
+
+  //Funcion Obtencion de datos de html dateTime
+  compareWith3(o1:any, o2:any) {
+    return o1 && o2 ? o1.id === o2.id : o1 === o2;
+  };
+
+  handleChange3(ev:any) {
+    this.currentFood3 = ev.target.value;
+    this.test_print=this.currentFood3;
+    this.test_print=this.test_print; 
+    
+ 
+    
+    console.log('Input dataa '+this.test_print.replace('T',' '))
+  };
+//Funcion Obtencion de datos de html dateTime
+
+
+
+//Funcion Listar Espacios Comunes (APISERVICE)
+async getEspacios(){
+  let that= this;
+  try{
+    const response_espacio: any = await that.api.listEspacios();
+    if (response_espacio.lenght != 0){
+      console.log('Funcionó :'+response_espacio)
+      this.espaciosComunes=response_espacio;
+    }
+
+  }catch(error){
+    console.log('Error Api !, ='+error)
+  }
 }
+//Funcion Listar Espacios Comunes (APISERVICE)
+
+
+//Funcion de validacion de ingreso de datos, corrector de flujo de informacion 
+mostrar_datos(){
+  if(this.idComunidad != ''){
+    this.presentToast('Seleccionaste una comunidad '+this.currentFood.nombre)
+  
+    
+    this.esVacio=true;
+  }else{
+    this.presentToast('Debes seleccionar una comunidad para continuar ')
+  }
+}
+//Funcion de validacion de ingreso de datos, corrector de flujo de informacion 
+
+
+
+
+}
+
+//Logica Pagina Tab1
